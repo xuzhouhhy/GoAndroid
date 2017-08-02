457452ab20d60d7de4237919c2a40aa00a281ab5
xuzhouhhy <xuzhouhhy@126.com>
2017-08-02 06:56:17 -0700

    新建工程代码

---
+++ source/GoAndroid/app/src/main/java/com/xuzhouhhy/goandroid/customview/CustomViewActivity.java
@@ -0,0 +1,20 @@
+package com.xuzhouhhy.goandroid.customview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xuzhouhhy.goandroid.R;
+
+/**
+ * Created by xuzhouhhy on 2017/8/1.
+ */
+
+public class CustomViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_view);
+    }
+}
