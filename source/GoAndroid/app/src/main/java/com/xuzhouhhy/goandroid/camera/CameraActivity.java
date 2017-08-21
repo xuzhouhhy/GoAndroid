c5c66545ee40cb93d6b0beb2307f43fa5d04a1ad
hanhongyun <hongyun_han@huacenav.com>
2017-08-21 15:19:21 +0800

    声明cameraactivity组件

---
+++ source/GoAndroid/app/src/main/java/com/xuzhouhhy/goandroid/camera/CameraActivity.java
@@ -0,0 +1,14 @@
+package com.xuzhouhhy.goandroid.camera;
+
+import android.app.Activity;
+
+/**
+ * custom camera
+ * Created by xuzhouhhy on 2017/8/21.
+ */
+
+public class CameraActivity extends Activity {
+
+    
+
+}
