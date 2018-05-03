d11bf1bd0ef98c5052022a6a5677b7a65c7b1bfa
hanhongyun <henry.han@ximalaya.com>
2018-05-03 14:07:22 +0800

    1调整toast的信息显示行数；2重命名toast布局

--- source/ImageToast/app/src/main/java/com/xuzhouhhy/imagetoast/MainActivity.java
+++ source/ImageToast/app/src/main/java/com/xuzhouhhy/imagetoast/MainActivity.java
@@ -1,11 +1,11 @@
 package com.xuzhouhhy.imagetoast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.RotateAnimation;
+import android.view.animation.LinearInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,33 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Animation animation = AnimationUtils.loadAnimation(this,R.animator.animation_rotate);
+        Animation animation = AnimationUtils.loadAnimation(this, R.animator.animation_rotate);
+        LinearInterpolator lin = new LinearInterpolator();
+        animation.setInterpolator(lin);
         findViewById(R.id.loading).startAnimation(animation);
     }
 
-    public void onSuccess(View view){
-        ToastUtil.showSuccess(this);
+    public void onSuccess(View view) {
+        ToastUtil.showSuccess(this, R.string.test_success);
     }
 
-    public void onLoading(View view){
+    public void onLoading(View view) {
         ToastUtil.showLoading(this);
     }
 
-    public void onFail(View view){
-        ToastUtil.showFail(this);
+    public void onFail(View view) {
+        ToastUtil.showFail(this, R.string.test_fail);
 
     }
 
-    public void onWarning(View view){
+    public void onWarning(View view) {
 
-        ToastUtil.showWarning(this);
+        ToastUtil.showWarning(this, R.string.test_warn);
 
     }
 
-    public void onFinish(View view){
+    public void onFinish(View view) {
 
-        ToastUtil.showFinish(this);
-
+        ToastUtil.showMsg(this, R.string.message_test);
     }
 
 
--- source/ImageToast/app/src/main/java/com/xuzhouhhy/imagetoast/ToastUtil.java
+++ source/ImageToast/app/src/main/java/com/xuzhouhhy/imagetoast/ToastUtil.java
@@ -4,50 +4,54 @@ import android.content.Context;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ToastUtil {
 
-    public static void showSuccess(Context context) {
+    public static void showSuccess(Context context, int stringId) {
         Toast toast = new Toast(context);
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_toast_success, null);
-        toast.setView(view);  toast.setDuration(Toast.LENGTH_LONG);
+        View view = LayoutInflater.from(context).inflate(R.layout.toast_success_layout, null);
+        ((TextView) view.findViewById(R.id.toast_text)).setText(stringId);
+        toast.setView(view);
+        toast.setDuration(Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
     }
 
     public static void showLoading(Context context) {
         Toast toast = new Toast(context);
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_toast_loading, null);
+        View view = LayoutInflater.from(context).inflate(R.layout.toast_loading_layout, null);
         toast.setView(view);
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
     }
 
-    public static void showWarning(Context context) {
+    public static void showWarning(Context context, int stringId) {
         Toast toast = new Toast(context);
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_toast_warning, null);
+        View view = LayoutInflater.from(context).inflate(R.layout.toast_warning_layout, null);
+        ((TextView) view.findViewById(R.id.toast_text)).setText(stringId);
         toast.setView(view);
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
     }
 
-    public static void showFinish(Context context) {
+    public static void showMsg(Context context, int stringId) {
         Toast toast = new Toast(context);
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_toast_message, null);
+        View view = LayoutInflater.from(context).inflate(R.layout.toast_message_layout, null);
+        ((TextView) view.findViewById(R.id.toast_text)).setText(stringId);
         toast.setView(view);
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
     }
 
-    public static void showFail(Context context) {
+    public static void showFail(Context context, int stringId) {
         Toast toast = new Toast(context);
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_toast_fail, null);
+        View view = LayoutInflater.from(context).inflate(R.layout.toast_fail_layout, null);
+        ((TextView) view.findViewById(R.id.toast_text)).setText(stringId);
         toast.setView(view);
         toast.setDuration(Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER, 0, 0);
